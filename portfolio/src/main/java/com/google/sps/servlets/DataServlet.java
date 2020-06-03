f0b608c28290b81c2b5424d714acdc767804c6e1
Charles <chiderajnr@gmail.com>
2020-06-03 18:41:48 +0000

    Enable json formatting and parsing into webpage

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,6 +19,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson; //Convert json to string
+import java.util.ArrayList; //To Enable Arraylist functionality through its class 
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -26,7 +28,22 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Charles Ogbogu!");
+    
+    ArrayList<String> storeCommentList = new ArrayList<String>();
+    storeCommentList.add("My name is Charles Ogbogu");
+    storeCommentList.add("I have learned so much in a short time and trying to test this program.");
+    storeCommentList.add("Who knows what I might learn tomorrow?");
+
+    String json = convertToJsonUsingGson(storeCommentList);
+
+    response.setContentType("application/json");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJsonUsingGson(ArrayList storeCommentList) {
+    Gson gson = new Gson();
+    String json = gson.toJson(storeCommentList);
+    return json;
   }
 }
+
