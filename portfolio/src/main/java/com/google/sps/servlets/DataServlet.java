5c509069665bab6320ea69d336998008833428a3
Charles <chiderajnr@gmail.com>
2020-06-05 21:26:38 +0000

    Enable deletion of comments and display preferred number of comments for user

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -40,7 +40,10 @@ public class DataServlet extends HttpServlet {
 
     PreparedQuery results = datastore.prepare(query);
 
+    int userCommentMax = maxNumberOfCommentsToDisplay(request);
+
     ArrayList<String> userCommentList = new ArrayList<>();
+
     for (Entity entity : results.asIterable()) {
       long id = (long) entity.getKey().getId();
       String firstName = (String) entity.getProperty("FirstName");
@@ -49,13 +52,13 @@ public class DataServlet extends HttpServlet {
       String messageDescription = (String) entity.getProperty("Message");
       long timestamp = (long) entity.getProperty("Timestamp");
 
-      String myUserComment = "Name: " + firstName + " " + lastName + "\n" + "ID: " + id + "\n Subject: " + subjectText + "\n" + messageDescription + "\n" + id;
+      String myUserComment = "Name: " + firstName + " " + lastName + "\n" + "ID: " + id + "\n Subject: " + subjectText + "\n" + messageDescription + "\n" + timestamp;
       userCommentList.add(myUserComment);
     }
 
     Gson gson = new Gson();
 
-    response.setContentType("application/json;");
+    response.setContentType("application/json");
     response.getWriter().println(gson.toJson(userCommentList));
   }
 
@@ -70,18 +73,46 @@ public class DataServlet extends HttpServlet {
     long timestamp = System.currentTimeMillis();
 
     // Initiate the Datastore service for storage of entity created
+    
     Entity commentEntity = new Entity("UserComment");
     commentEntity.setProperty("FirstName", firstName);
     commentEntity.setProperty("LastName", lastName);
     commentEntity.setProperty("Subject", subjectText);
     commentEntity.setProperty("Message", messageDescription);
     commentEntity.setProperty("Timestamp", timestamp);
-
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(commentEntity);
+    
+    // Ensure and avoid all empty entry values
+    if (!"".equals(firstName) && !"".equals(lastName)) { // Yoda expression to avoid null pointers by using .equals to compare strings in Java
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
+    } else {
+        response.sendRedirect("contact.html");
+    }
 
     // Redirect back to the Contact page.
     response.sendRedirect("contact.html");
     
   }
+
+  public int maxNumberOfCommentsToDisplay(HttpServletRequest request) {
+    String userNumberOfComment = request.getParameter("comment-number");
+
+    // Convert the input to an int.
+    int userCommentMax;
+    try {
+      userCommentMax = Integer.parseInt(userNumberOfComment);
+    } catch (NumberFormatException e) {
+      System.err.println("Could not convert to int: " + userNumberOfComment);
+      return 3;
+    }
+
+    // Check that the input is between 1 and 5.
+    if (userCommentMax < 1 || userCommentMax > 5) {
+      System.err.println("Player choice is out of range: " + userNumberOfComment);
+      return 3;
+    }
+    
+    return userCommentMax;
+  }
+
 }
